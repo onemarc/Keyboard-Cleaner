7f6f6b1d76b0146446cf94c524271b436840ef76
One <110691813+onemarc@users.noreply.github.com>
2023-11-30 22:28:27 +0200

    main commit

---
+++ Keyboard Cleaner/MyWindow.cpp
@@ -0,0 +1,17 @@
+﻿#include "MyWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+extern "C" {
+	__declspec(dllimport) int __stdcall BlockInput(int fBlockIt);
+}
+
+[STAThreadAttribute]
+
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	OneKeyBoardCleaner::MyWindow window;
+	Application::Run(% window);
+}
---
+++ Keyboard Cleaner/MyWindow.h
@@ -0,0 +1,219 @@
+﻿#pragma once
+
+
+#include <windows.h>
+
+
+extern "C" {
+	__declspec(dllimport) int __stdcall BlockInput(int fBlockIt);
+}
+
+namespace OneKeyBoardCleaner {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	public ref class MyWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MyWindow(void)
+		{
+			InitializeComponent();
+			this->MaximumSize = System::Drawing::Size(807, 353);
+			this->MinimumSize = System::Drawing::Size(807, 353);
+
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			isInitialImage = true;
+		}
+
+	protected:
+		~MyWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+
+	private: System::Windows::Forms::BindingSource^ bindingSource1;
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+
+	private: System::ComponentModel::IContainer^ components;
+
+		   bool isKeyboardBlocked;
+		   bool isInitialImage;
+
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+
+
+
+
+
+	private:
+		bool isImageChanged = false;
+	
+
+#pragma region Windows Form Designer generated code
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyWindow::typeid));
+			this->bindingSource1 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Consolas", 11, System::Drawing::FontStyle::Bold));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(255, 81);
+			this->label1->Name = L"label1";
+			this->label1->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->label1->Size = System::Drawing::Size(804, 104);
+			this->label1->TabIndex = 3;
+			this->label1->Text = resources->GetString(L"label1.Text");
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->label1->Click += gcnew System::EventHandler(this, &MyWindow::label1_Click);
+			// 
+			// button1
+			// 
+			this->button1->FlatAppearance->BorderSize = 10;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Consolas", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::SystemColors::WindowFrame;
+			this->button1->Location = System::Drawing::Point(38, 201);
+			this->button1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(177, 56);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"START CLEANING";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyWindow::button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(1, 48);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(248, 129);
+			this->pictureBox1->TabIndex = 5;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyWindow::pictureBox1_Click_1);
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(1, 48);
+			this->pictureBox2->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(248, 129);
+			this->pictureBox2->TabIndex = 6;
+			this->pictureBox2->TabStop = false;
+			// 
+			// MyWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 28);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoScroll = true;
+			this->BackColor = System::Drawing::SystemColors::WindowFrame;
+			this->ClientSize = System::Drawing::Size(795, 318);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->pictureBox2);
+			this->Cursor = System::Windows::Forms::Cursors::Default;
+			this->Font = (gcnew System::Drawing::Font(L"Consola Mono", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->ImeMode = System::Windows::Forms::ImeMode::On;
+			this->Margin = System::Windows::Forms::Padding(5, 4, 5, 4);
+			this->Name = L"MyWindow";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"KeyBoard Cleaner";
+			this->Load += gcnew System::EventHandler(this, &MyWindow::MyWindow_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyWindow_Load(System::Object^ sender, System::EventArgs^ e) {
+		button1->TabStop = false;
+		label1->Focus();
+	}
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void pictureBox2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void pictureBox2_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		label1->Focus();
+
+		if (isKeyboardBlocked) {
+			button1->Text = "START CLEANING";
+			UnblockKeyboard();
+		}
+		else {
+			button1->Text = "STOP CLEANING";
+			BlockKeyboard();
+		}
+
+		// Here we change the image.
+		isKeyboardBlocked = !isKeyboardBlocked;
+
+		if (isInitialImage) {
+			pictureBox1->SendToBack();
+			pictureBox2->BringToFront();
+		}
+		else {
+			pictureBox2->SendToBack();
+			pictureBox1->BringToFront();
+		}
+
+		
+		isInitialImage = !isInitialImage;
+	}
+		   // Here we block the keyboard.
+		   private:
+		   	
+		   	void BlockKeyboard() {
+		   		BlockInput(TRUE);
+		   	}
+
+		   	
+		   	void UnblockKeyboard() {
+		   		BlockInput(FALSE);
+}
+private: System::Void pictureBox2_Click_2(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void pictureBox1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
